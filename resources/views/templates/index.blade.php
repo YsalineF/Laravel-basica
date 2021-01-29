b9699b780f02bd1b55c8389f69633d2adcc9e45e
Mézz <ysaline.franquet@gmail.com>
2021-01-29 15:19:36 +0100

    Intégration et découpe du template

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+
+  <head>
+    @include('templates.partials._head')
+  </head>
+
+  <body>
+
+    @include('templates.partials._header')
+
+    @include('templates.partials._main')
+
+  	@include('templates.partials._footer')
+
+    @include('templates.partials._scripts')
+
+  </body>
+</html>
