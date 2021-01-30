b0317a93816c85ae368f4334cf80dc23dacf78df
Mézz <ysaline.franquet@gmail.com>
2021-01-30 13:03:03 +0100

    Modification de la découpe du template
    Ajout d'un partial _nav dans le partial _header

--- resources/views/templates/partials/_header.blade.php
+++ resources/views/templates/partials/_header.blade.php
@@ -9,13 +9,6 @@
             </button>
             <a class="navbar-brand" href="index.html"><img src="{{ asset('assets/img/logo.png') }}" alt="Basica"></a>
         </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="portfolio.html">Portfolio</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="contact-us.html">Contact</a></li>
-            </ul>
-        </div>
+        @include('templates.partials._nav')
     </div>
 </header><!--/header-->
---
+++ resources/views/templates/partials/_nav.blade.php
@@ -0,0 +1,8 @@
+<div class="collapse navbar-collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="portfolio.html">Portfolio</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li><a href="contact-us.html">Contact</a></li>
+    </ul>
+</div>
