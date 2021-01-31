7373daeff2c436d3e9df1afee234b772fb0b1ca2
Mézz <ysaline.franquet@gmail.com>
2021-01-31 19:04:45 +0100

    Modification de la class active sur les éléments du nav

--- resources/views/templates/partials/_nav.blade.php
+++ resources/views/templates/partials/_nav.blade.php
@@ -1,8 +1,8 @@
 <div class="collapse navbar-collapse">
     <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('works.index') }}">Portfolio</a></li>
-        <li><a href="{{ route('posts.index') }}">Blog</a></li>
-        <li><a href="{{ route('contact') }}">Contact</a></li>
+        <li class="{{ Route::currentRouteName() == 'home' ? 'active' : ''}}"><a href="{{ route('home') }}">Home</a></li>
+        <li class="{{ Route::currentRouteName() == 'works.index' ? 'active' : ''}}"><a href="{{ route('works.index') }}">Portfolio</a></li>
+        <li class="{{ Route::currentRouteName() == 'posts.index' ? 'active' : ''}}"><a href="{{ route('posts.index') }}">Blog</a></li>
+        <li class="{{ Route::currentRouteName() == 'contact' ? 'active' : ''}}"><a href="{{ route('contact') }}">Contact</a></li>
     </ul>
 </div>
