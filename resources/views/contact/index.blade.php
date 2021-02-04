e0e8a662fbc196d3f4f8c2a53da72832a442cbd9
Mézz <ysaline.franquet@gmail.com>
2021-02-04 03:02:50 +0100

    Modification de l'adresse de la Google Map de la page de contact

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -14,12 +14,7 @@
     <div class="col-sm-12" style="padding:0;">
       <!-- Map -->
       <div id="contact-us-map">
-        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
-        <br />
-        <small>
-          <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
-        </small>
-      </iframe>
+        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2531.8491797636043!2d5.681296815900428!3d50.61133998363957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0f41cf16f5fcf%3A0xcb72bfa803234f92!2sRue%20Charles%20Deli%C3%A8ge%209%2C%204623%20Fl%C3%A9ron!5e0!3m2!1sfr!2sbe!4v1612379630611!5m2!1sfr!2sbe"></iframe>
     </div>
     <!-- End Map -->
   </div>
@@ -33,7 +28,7 @@
     <div class="col-sm-6">
       <!-- Contact Info -->
       <p class="contact-us-details">
-        <b>Address:</b> 123 Fake Street, LN1 2ST, London, United Kingdom<br/>
+        <b>Address:</b> 9 Rue Charles Deliège, 4623 Fléron, Belgium<br/>
         <b>Phone:</b> +44 123 654321<br/>
         <b>Fax:</b> +44 123 654321<br/>
         <b>Email:</b> <a href="mailto:getintoutch@yourcompanydomain.com">getintoutch@yourcompanydomain.com</a>
