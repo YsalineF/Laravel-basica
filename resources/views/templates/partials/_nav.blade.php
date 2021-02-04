3e22ee839ac12396ccc5d4cd5e4ed5e76e5efdef
Mézz <ysaline.franquet@gmail.com>
2021-02-04 20:02:37 +0100

    Ajout liens connexion et register au nav

--- resources/views/templates/partials/_nav.blade.php
+++ resources/views/templates/partials/_nav.blade.php
@@ -4,5 +4,7 @@
         <li class="{{ Route::currentRouteName() == 'works.index' ? 'active' : ''}} {{ Route::currentRouteName() == 'works.show' ? 'active' : ''}}"><a href="{{ route('works.index') }}">Portfolio</a></li>
         <li class="{{ Route::currentRouteName() == 'posts.index' ? 'active' : ''}} {{ Route::currentRouteName() == 'posts.show' ? 'active' : ''}}"><a href="{{ route('posts.index') }}">Blog</a></li>
         <li class="{{ Route::currentRouteName() == 'contact' ? 'active' : ''}}"><a href="{{ route('contact') }}">Contact</a></li>
+        <li><a href="{{ route('login') }}">Connexion</a></li>
+        <li><a href="{{ route('register') }}">Register</a></li>
     </ul>
 </div>
